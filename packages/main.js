e1735769e31195bbe769b4ff17fbc024efd00a96
Ann Edvinsson <20264858+AnnEdvinsson@users.noreply.github.com>
2017-11-22 13:42:24 +0100

    packages

---
+++ packages/main.js
@@ -0,0 +1,9 @@
+function myOpen(){
+  document.getElementById("slideid1").style.width="30%";
+  document.getElementById('buttonOpen').style.display="none";
+}
+//
+function myClose(){
+  document.getElementById('slideid1').style.width="0%";
+  document.getElementById('buttonOpen').style.display="block";
+}
