9aa0165900f550421af6f1018ac8d380c9d64765
Ann Edvinsson <20264858+AnnEdvinsson@users.noreply.github.com>
2017-11-22 13:44:58 +0100

    Delete main.js

--- packages/main.js
+++
@@ -1,9 +0,0 @@
-function myOpen(){
-  document.getElementById("slideid1").style.width="30%";
-  document.getElementById('buttonOpen').style.display="none";
-}
-//
-function myClose(){
-  document.getElementById('slideid1').style.width="0%";
-  document.getElementById('buttonOpen').style.display="block";
-}
